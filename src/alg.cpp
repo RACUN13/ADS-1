3ad0bc889387a092cef720636f7ca09ba76789ee
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-04 19:30:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,21 +12,38 @@ bool checkPrime(uint64_t value) {
       if(value % i == 0) {
         return false;
       }
-    }
+    
       else {
         return true;
       }
+    }
   }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  int value = 2;
+  int bre = 0;
+  while (bre < n) {
+    if (checkPrime(value++)) {
+      bre++;
+    }
+  }
+  return --value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  value++;
+  while (checkPrime(value) == false) {
+    value++;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum = 0;
+  for(int i = 2; i < hbound; i++) {
+    if(checkPrime(i) == true)
+      sum = sum + i;
+  }
+  return sum;
 }
