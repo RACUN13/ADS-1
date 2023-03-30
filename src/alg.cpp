87b5ee6198a8a76a7aa9118e38d523274c554e34
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-30 22:58:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,14 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value == 0 || value == 1) {
-    return false;
-  } else {
-    for (int i = 2; i <= value; ++i) {
-      if (value % i == 0) {
+  if (value > 1) {
+    for (int i = 2; i < value; ++i)
+      if (value % i == 0)
         return false;
-      } else {
-        return true;
-      }
-    }
-    }
+    return true;
+  }
+    else
+      return false;
 }
 
 uint64_t nPrime(uint64_t n) {
