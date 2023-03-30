49cce9a312b44d8cea057f9af461d36ffef9c27e
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-31 00:20:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,9 +33,12 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+  hbound -= 1;
   int sum = 0;
-  for (int i = 2; i < hbound; i++)
-    if (checkPrime(i) == true)
+  for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i) == true) {
       sum = sum + i;
+    }
+  }
   return sum;
 }
