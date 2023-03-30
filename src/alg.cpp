18b66a1854c6c68a24656626831a3d4f9a39a910
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-31 00:25:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,32 +13,27 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int value = 0;
-  int bre = 0;
-  while (n > 0) {
-        if (checkPrime(bre)) {
-            value = bre;
-            n -= 1;
+    uint64_t value = 0, bre = 0;
+    for (uint64_t i = 2; bre < n; i++) {
+        if (checkPrime(i)) {
+          value = i;
+          bre++;
         }
-        bre += 1;
-  }
+    }
     return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value += 1;
-  while (checkPrime(value) == false)
-    value++;
-  return value;
+    value = value + 1;
+    while (!checkPrime(value))
+      value++;
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  hbound -= 1;
-  int sum = 0;
-  for (int i = 2; i < hbound; i++) {
-    if (checkPrime(i) == true) {
-      sum = sum + i;
-    }
-  }
-  return sum;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++)
+        if (checkPrime(i))
+          sum += i;
+    return sum;
 }
